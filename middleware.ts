992814df11e123f2bb106949c629d493ff986c89
zura <zuramanagadze10@gmail.com>
2024-11-20 15:36:27 +0400

    09.02-delete middleware.ts for experiment

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-export { auth as middleware } from "./auth";
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"], // Apply middleware to all pages except API routes and static files
-};
